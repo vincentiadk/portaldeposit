78779d5544ee34f1d408d51a49e71e3e28a0128d
vincentiadk <50853015+vincentiadk@users.noreply.github.com>
2019-10-10 13:04:47 +0700

    Update home.blade.php

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -3,7 +3,7 @@
 <section id="intro">
     <div class="container">
         <div class="row">
-           <div class="span12">
+         <div class="span12">
             <!-- start flexslider -->
             <div class="flexslider">
                 <ul class="slides">
@@ -160,21 +160,22 @@
                 <span class="section-title uppercase white">Koleksi Deposit Terbaru</span>
                 <div class="testmonial_slider">
                     <ul class="slides">
-                     @foreach ($depositNewest->chunk(4) as $depost4) 
-                     <li>
-                        <div class="row">
-                        @foreach($depost4->chunk(2) as $depost2)
-                        @foreach($depost2 as $data)
-                        @php 
-                        $col = $data->collections->where('noinduk_deposit','!=',null)->first();
-                        $titles = $data->catalog_ruas->where('tag','245')->first()->value;
-                        $title = "";
-                        if(preg_match('/[$]a(.*?)[$]/',$titles, $match)==1) 
-                        {
-                            $title = trim($match[1]);
-                        }
-                        @endphp
-                        
+                        @foreach ($depositNewest->chunk(4) as $depost4) 
+                        <li>
+                           @foreach($depost4->chunk(2) as $depost2)
+                           <div class="row">
+
+                            @foreach($depost2 as $data)
+                            @php 
+                            $col = $data->collections->where('noinduk_deposit','!=',null)->first();
+                            $titles = $data->catalog_ruas->where('tag','245')->first()->value;
+                            $title = "";
+                            if(preg_match('/[$]a(.*?)[$]/',$titles, $match)==1) 
+                            {
+                                $title = trim($match[1]);
+                            }
+                            @endphp
+
                             <div class="mini-layout fluid span6 ebook">
                                 <div class="mini-layout-sidebar">
                                     @if($data->coverurl != null)
@@ -191,11 +192,11 @@
                                         </a>
                                         @endif
                                     </p>
-                                    Tgl Terima <span class="date"><i class="icon-calendar"></i> {{$col->created_at}}</span> 
+                                    Tgl Terima <span class="date"><i class="icon-calendar"></i> {{$col->created_at}} </span> 
                                 </div>
                             </div>
-                        
-                        @endforeach
+
+                            @endforeach
                         </div>
                         @endforeach
                     </li>
