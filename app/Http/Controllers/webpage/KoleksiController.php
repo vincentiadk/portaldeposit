b736db02122f2170221702ea51bbed7f948da26f
vincentiadk <50853015+vincentiadk@users.noreply.github.com>
2019-10-18 07:45:42 +0700

    Update KoleksiController.php

--- app/Http/Controllers/webpage/KoleksiController.php
+++ app/Http/Controllers/webpage/KoleksiController.php
@@ -53,7 +53,24 @@ class KoleksiController extends Controller
 
     private function getByPage(Request $request)
     {
-        $data = Catalog::whereHas('collections',function($query) {
+        $type = request('type');
+        $search = request('search');
+        $collectionArray = Collection::where('collections.category_id', 4)
+            ->select('catalog_id')
+            ->join('master_publisher','master_publisher.publisher_id','collections.publisher_id')
+            ->groupby('catalog_id','createdate');
+        if($type != ""){
+            $collectionArray->where('type_of_publisher',$type);
+        }
+        if($search != ""){
+            $collectionArray->where(DB::Raw('lower(title)'), 'like', '%'.strtolower($search).'%');
+        }
+        $collectionArray->get()->toArray();
+        $catIds = array_column($collectionArray,'catalog_id');
+        $data = Catalog::whereIn('id',$catIds);
+        //->paginate(16)->appends(\Request::only(['search','type']))->setPath('');
+
+        /*$data = Catalog::whereHas('collections',function($query) {
             $query->where('category_id',4);
         });
         $type = request('type');
@@ -69,7 +86,7 @@ class KoleksiController extends Controller
                 $query->orwhere(DB::Raw('lower(title)'), 'like', '%'.strtolower($search).'%');
             });
         }
-
+        */
         return $data->paginate(16)->appends(\Request::only(['search','type']))->setPath('');
     }
 
