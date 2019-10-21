ca63f9bec5e106d53e7db2af10e2b35f743f2090
vincentiadk <50853015+vincentiadk@users.noreply.github.com>
2019-10-21 13:40:24 +0700

    Update AbstractWC.php

--- app/Http/Controllers/Admin/AbstractWC.php
+++ app/Http/Controllers/Admin/AbstractWC.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use DataTables;
 use App\Catalog;
 use App\AbstractCat;
