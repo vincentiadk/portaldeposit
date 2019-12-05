6122fd3955dfee72908df872c217991ca74e6234
vincentiadk <50853015+vincentiadk@users.noreply.github.com>
2019-12-05 20:11:38 +0700

    All admin

--- app/Http/Controllers/Admin/PedomanWC.php
+++ app/Http/Controllers/Admin/PedomanWC.php
@@ -14,138 +14,133 @@ use App\Rule;
 
 class PedomanWC extends Controller
 {
-  function pedomanView () 
-  {
-    return view('admin.pedoman.pedoman');
-  }
-
-  function pedomanTambahView () 
-  {
-    return view('admin.pedoman.pedoman-detail')->with(["mode"=>"tambah","action"=>"/bo/pedoman/tambah"]);
-  }
-
-  function pedomanDetailView ($id) 
-  {
-    $data = Rule::where('id', $id)->first();
-    return view('admin.pedoman.pedoman-detail')->with(["id"=>$id, 'data'=>$data,"mode"=>"detail","action"=>"/bo/pedoman/update"]);
-  }
-
-  function pedomanUpdate(Request $req) 
-  {
-    $user = Auth::user();
-
-    $slug = 'rule/'.str_replace(' ', '-', strtolower($req->title));
-
-    if (isset($req->simpanpub)) $status = "published";
-    else $status = "not published";
-
-    $array_to_input = [
-      "type" => $req->type,
-      "title" => $req->title,
-      "slug" => $slug,
-      "datetime" => now(),
-      "is_comment" => $req->is_comment,
-      "description" => $req->description,
-      "status" => $status,
-      "updated_by" => $user->id,
-      "updated_at" => now()
-    ];
-
-    if($req->hasFile('dokumen')){
-      $dokumen = $req->dokumen;
-      $file_name = Uuid::generate()->string.'.'.strtolower($dokumen->getClientOriginalExtension());
-      if (strtolower($dokumen->getClientOriginalExtension())== 'pdf' && $dokumen->getSize()<= 60000000) {
-        $oldName = Rule::find($req['id'])->files;
-        if ($oldName == null) {
-          $array_to_input['files']=$file_name;
-
-        }else{
-          $file_name = $oldName;
-        }
-        $dokumen->storeAs('public/pdf', $file_name);
-      }
+    function pedomanView () 
+    {
+        return view('admin.pedoman.pedoman');
     }
 
-    $res = Rule::where('id', $req['id'])->update($array_to_input);
-
-    if ($res == 0) 
+    function pedomanTambahView () 
     {
-      return back()->with(["status"=>0]);    
+        return view('admin.pedoman.pedoman-detail')->with(["mode"=>"tambah","action"=>"/bo/pedoman/tambah"]);
     }
-    else
+
+    function pedomanDetailView ($id) 
     {
-      if($req->hasFile('dokumen')){
-        $dokumen = $req->dokumen;
-        $file_name = Uuid::generate()->string.'.'.strtolower($dokumen->getClientOriginalExtension());
-          if (strtolower($dokumen->getClientOriginalExtension())== 'pdf' && $dokumen->getSize()<= 60000000) {
-           $dg = Rule::find($req['id']);
-            if ($dg->files == null) {
-              $dg->files = $file_name;
-              $dg->save();
-            }else{
-              $file_name = $dg->files;
+        $data = Rule::where('id', $id)->first();
+        return view('admin.pedoman.pedoman-detail')->with(["id"=>$id, 'data'=>$data,"mode"=>"detail","action"=>"/bo/pedoman/update"]);
+    }
+
+    function pedomanUpdate(Request $req) 
+    {
+        $user = Auth::user();
+
+        $slug = 'rule/'.str_replace(' ', '-', strtolower($req->title));
+
+        if (isset($req->simpanpub)) $status = "published";
+        else $status = "not published";
+
+        $array_to_input = [
+            "type" => $req->type,
+            "title" => $req->title,
+            "slug" => $slug,
+            "datetime" => now(),
+            "is_comment" => $req->is_comment,
+            "description" => $req->description,
+            "status" => $status,
+            "updated_by" => $user->id,
+            "updated_at" => now()
+        ];
+
+        if($req->hasFile('dokumen')){
+            $dokumen = $req->dokumen;
+            $file_name = Uuid::generate()->string.'.'.strtolower($dokumen->getClientOriginalExtension());
+            if (strtolower($dokumen->getClientOriginalExtension())== 'pdf' && $dokumen->getSize()<= 60000000) {
+                $oldName = Rule::find($req['id'])->files;
+                if ($oldName == null) {
+                    $array_to_input['files']=$file_name;
+
+                }else{
+                    $file_name = $oldName;
+                }
+                $dokumen->storeAs('public/pdf', $file_name);
             }
-            $dokumen->storeAs('public/pdf', $file_name);
-          }
         }
-      return back()->with(["status"=>1]);
-    }   
-  }
 
-  function pedomanTambah(Request $req) {
-    $user = Auth::user();
+        $res = Rule::where('id', $req['id'])->update($array_to_input);
 
-    $slug = 'rule/'.str_replace(' ', '-', strtolower($req->title));
-    
-    if (isset($req->simpanpub)) $status = "published";
-    else $status = "not published";
+        if ($res == 0) {
+            return back()->with(["status"=>0]);    
+        }  else {
+            if($req->hasFile('dokumen')){
+                $dokumen = $req->dokumen;
+                $file_name = Uuid::generate()->string.'.'.strtolower($dokumen->getClientOriginalExtension());
+                if (strtolower($dokumen->getClientOriginalExtension())== 'pdf' && $dokumen->getSize()<= 60000000) {
+                    $dg = Rule::find($req['id']);
+                    if ($dg->files == null) {
+                        $dg->files = $file_name;
+                        $dg->save();
+                    } else {
+                        $file_name = $dg->files;
+                    }
+                    $dokumen->storeAs('public/pdf', $file_name);
+                }
+            }
+            return back()->with(["status"=>1]);
+        }   
+    }
 
-    $array_to_input = [
-      "type" => $req->type,
-      "title" => $req->title,
-      "slug" => $slug,
-      "datetime" => now(),
-      "is_comment" => $req->is_comment,
-      "description" => $req->description,
-      "status" => $status,
-      "created_by" => $user->id,
-      "created_at" => now()
-    ];
-
-    
-
-
-
-    $data["id"] = Rule::insertGetId($array_to_input);
-
-    if ($data["id"] != null) 
+    function pedomanTambah(Request $req) 
     {
-      if($req->hasFile('dokumen')){
-        $dokumen = $req->dokumen;
-        $file_name = Uuid::generate()->string.'.'.strtolower($dokumen->getClientOriginalExtension());
-        if (strtolower($dokumen->getClientOriginalExtension())== 'pdf' && $dokumen->getSize()<= 60000000) {
-          $dg = Rule::find($data["id"] );
-          $dg->files = $file_name;
-          $dg->save();
-          $dokumen->storeAs('public/pdf', $file_name);
+        $user = Auth::user();
+
+        $slug = 'rule/'.str_replace(' ', '-', strtolower($req->title));
+
+        if (isset($req->simpanpub)) $status = "published";
+        else $status = "not published";
+
+        $array_to_input = [
+            "type" => $req->type,
+            "title" => $req->title,
+            "slug" => $slug,
+            "datetime" => now(),
+            "is_comment" => $req->is_comment,
+            "description" => $req->description,
+            "status" => $status,
+            "created_by" => $user->id,
+            "created_at" => now()
+        ];
+
+
+
+
+
+        $data["id"] = Rule::insertGetId($array_to_input);
+
+        if ($data["id"] != null) {
+            if($req->hasFile('dokumen')){ 
+                $dokumen = $req->dokumen;
+                $file_name = Uuid::generate()->string.'.'.strtolower($dokumen->getClientOriginalExtension());
+                if (strtolower($dokumen->getClientOriginalExtension())== 'pdf' && $dokumen->getSize()<= 60000000) {
+                    $dg = Rule::find($data["id"] );
+                    $dg->files = $file_name;
+                    $dg->save();
+                    $dokumen->storeAs('public/pdf', $file_name);
+                }
+            }
+            return back()->with(["status"=>1]);    
+        }  else {
+            return back()->with(["status"=>0]);
         }
-      }
-      return back()->with(["status"=>1]);    
     }
-    else
+
+    function pedomanDelete (Request $request) 
     {
-      return back()->with(["status"=>0]);
-    }
-  }
+        $data = Rule::find($request->id);
+        if ($data) { 
+            File::delete(storage_path('app/public/pdf/'.$data->files));
+            $data->delete();
 
-  function pedomanDelete (Request $request) 
-  {
-    $data = Rule::find($request->id);
-    if ($data) { 
-        File::delete(storage_path('app/public/pdf/'.$data->files));
-        $data->delete();
-
+        }
+        return redirect('/bo/pedoman')->with(["status"=>1]);
     }
-    return redirect('/bo/pedoman')->with(["status"=>1]);
-  }
 }
--- app/Http/Controllers/Admin/TentangWC.php
+++ app/Http/Controllers/Admin/TentangWC.php
@@ -11,52 +11,48 @@ use App\Rule;
 class TentangWC extends Controller
 {
 	public function tentangView(){
-	    $data = Rule::with('createdBy', 'updatedBy')->where('type', 'tentang')->first();
-	    return view('admin.tentang.tentang')->with(['data'=>$data, "action"=>"/bo/tentang/save"]);
+		$data = Rule::with('createdBy', 'updatedBy')->where('type', 'tentang')->first();
+		return view('admin.tentang.tentang')->with(['data'=>$data, "action"=>"/bo/tentang/save"]);
 	}
 
 	public function save(Request $req)
 	{
-	    $user = Auth::user();
+		$user = Auth::user();
 
-	    if (isset($req->simpan)){
-		    $array_to_input = [
-		      "type" => "tentang",
-		      "title" => "Tentang Kami",
-		      "slug" => "tentang/tentang-kami",
-		      "datetime" => now(),
-		      "is_comment" => 1,
-		      "description" => $req->description,
-		      "status" => "published",
-		      "created_by" => $user->id,
-		      "created_at" => now()
-		    ];
+		if (isset($req->simpan)){
+			$array_to_input = [
+				"type" => "tentang",
+				"title" => "Tentang Kami",
+				"slug" => "tentang/tentang-kami",
+				"datetime" => now(),
+				"is_comment" => 1,
+				"description" => $req->description,
+				"status" => "published",
+				"created_by" => $user->id,
+				"created_at" => now()
+			];
 
-		    $data = Rule::insertGetId($array_to_input);
+			$data = Rule::insertGetId($array_to_input);
 
 		    if ($data != null) $res =1; //success
 		    else $res = 0; //failed
-	    }
-	    else if(isset($req->update)){
-		    $array_to_input = [
-		      "type" => "tentang",
-		      "title" => "Tentang Kami",
-		      "slug" => "tentang/tentang-kami",
-		      "description" => $req->description,
-		      "updated_by" => $user->id,
-		      "updated_at" => now()
-		    ];
+		} else if(isset($req->update)){
+			$array_to_input = [
+				"type" => "tentang",
+				"title" => "Tentang Kami",
+				"slug" => "tentang/tentang-kami",
+				"description" => $req->description,
+				"updated_by" => $user->id,
+				"updated_at" => now()
+			];
 
-		    $res = Rule::where('id', $req->id)->update($array_to_input);
-	    }
+			$res = Rule::where('id', $req->id)->update($array_to_input);
+		}
 
-	    if ($res == 0) 
-	    {
-	      return back()->with(["status"=>0]);    
-	    }
-	    else
-	    {
-	      return back()->with(["status"=>1]);
-	    }  
+		if ($res == 0) {
+			return back()->with(["status"=>0]);    
+		} else {
+			return back()->with(["status"=>1]);
+		}  
 	}
 }
